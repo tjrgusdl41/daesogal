3b1ac6f91630c74dfa378a17bec2f64ba64adb56
tjrgusdl41 <63176593+tjrgusdl41@users.noreply.github.com>
2021-09-15 16:51:42 +0900

    20210915

---
+++ dasogal/src/main/java/com/nar/dasogal/DasogalApplication.java
@@ -0,0 +1,13 @@
+package com.nar.dasogal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasogalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasogalApplication.class, args);
+	}
+
+}
---
+++ dasogal/src/test/java/com/nar/dasogal/DasogalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nar.dasogal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasogalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
